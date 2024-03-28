cc652aced7030c876c07f50b2fff54da6e7b4783
racherdsvideos3 <164130973+racherdsvideos3@users.noreply.github.com>
2024-03-28 05:14:04 +0000

    CMABIOS EN EL MUNDO

---
+++ Minecraft-server/kubejs/startup_scripts/worldgen.js
@@ -0,0 +1,24 @@
+WorldgenEvents.remove(event => {
+  event.removeOres(props => {
+    props.worldgenLayer = 'underground_ores';
+    props.blocks = [
+      "railcraft:lead_ore",
+      "railcraft:deepslate_lead_ore",
+      "railcraft:nickel_ore",
+      "railcraft:deepslate_nickel_ore",
+      "railcraft:tin_ore",
+      "railcraft:deepslate_tin_ore",
+      "railcraft:silver_ore",
+      "railcraft:deepslate_silver_ore",
+      "railcraft:zinc_ore",
+      "railcraft:deepslate_zinc_ore",
+      "create:zinc_ore",
+      "create:deepslate_zinc_ore",
+      "occultism:silver_ore",
+      "occultism:silver_ore_deepslate",
+      "bigreactors:yellorite_ore",
+      "immersiveengineering:ore_aluminum",
+      "immersiveengineering:deepslate_ore_aluminum"
+    ]
+  })
+})
